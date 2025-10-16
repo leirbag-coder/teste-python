1163cd30e492f7cb0306a611a1eef979b38e8a90
leirbag-coder <gabrielscreed3@gmail.com>
2025-10-16 16:17:28 -0300

    Terceiro commit

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 print("olá mundo")
 nome = input("qual seu nome ?")
 idade = int(input("qual sua idade?"))
-           
-print(f"olá {nome}, você tem {idade} anos!")
+a = int(input("digie um número para a?"))
+b = int(input("digite um número para b?"))
+
+
+maioridade = ""
+if idade >= 21:
+    maioridade = "Maior de 21 anos!"
+elif idade >= 18:
+    maioridade = "Maior ou igual de 18 anos!"
+else:
+    maioridade = "Menor de 18 anos!"    
+"""
+def somar(a,b):
+    return a + b 
+print(f"o resultado da conta a + b = {somar(a,b)}")
+
+
+print(f"olá {nome}, você tem {idade} anos! Pelo visto você tem idade {maioridade}!") 
+
+for i in range(5):
+    print(f"Valor de i é igual à: {i}")
+"""
+
+frutas = ["Maçã", "Banana", "Uva"]
+for frutas in frutas:
+    print("Eu gosto de ", frutas)
